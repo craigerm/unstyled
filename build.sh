3efd8667e8093e63fd668123dce159f6cf56ff9a
Craig MacGregor <craigerm@gmail.com>
2013-04-21 02:13:07 -0400

    Use git describe when creating release zip for app store

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 mkdir dist -p
-filename=`date --rfc-3339=date`
-echo "Exporting to file dist/$filename.zip"
-if [ -f "dist/$filename.zip" ];
+filename=`(echo -n "dist/"; echo -n \`git describe\`; echo ".zip") | cat`
+
+echo "Exporting to file $filename"
+
+if [ -f "$filename" ];
 then
   echo "Can't export! The file already exists."
   exit
 fi
 
-git archive HEAD --format=zip > "dist/$filename.zip"
+git archive HEAD --format=zip > "$filename"
 echo "Done."
-
